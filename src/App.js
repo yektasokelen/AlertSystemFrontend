5704d4371ed8f5ed9bb48ddfc61b9641558da383
Yekta Sökelen <yektasokelen@gmail.com>
2019-08-23 12:57:29 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Graph from "./components/Graph";
+import AlertForm from "./components/AlertForm";
+import SituationTable from "./components/SituationTable";
+import { Navbar, Nav} from 'react-bootstrap';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import {LinkContainer} from "react-router-bootstrap";
+import {Link} from "react-router-dom";
+import UpdateData from "./components/UpdateData";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Router>
+                  <Navbar bg="light" variant="light">
+                      <Navbar.Brand><Link to="/alertform">AlertSystem</Link></Navbar.Brand>
+                      <Nav className='mr-auto'>
+                          <LinkContainer to="/alertform"><Nav.Link>ADD ALERT</Nav.Link></LinkContainer>
+                          <LinkContainer to="/alertlist"><Nav.Link>ALERT LIST</Nav.Link></LinkContainer>
+                      </Nav>
+                  </Navbar>
+            <Switch>
+              <Route exact path='/alertform' component={AlertForm}/>
+              <Route path='/alertlist' component={SituationTable}/>
+              <Route path='/graph/:id' component={Graph}/>
+              <Route path='/updatedata/:id' component={UpdateData}/>
+            </Switch>
+          </Router>
   );
 }
 
